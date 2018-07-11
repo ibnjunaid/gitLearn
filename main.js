4f8f392a2b0864c3ed708e4d5802444c67e01103
Osama <osamabinjunaid36@gmail.com>
2018-07-11 15:40:26 +0530

    Added add ()

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+
 callMyName = () => {
 for (let i = 0; i< 10; i++){
 		console.log('osama','\n');
@@ -6,3 +8,13 @@ for (let i = 0; i< 10; i++){
 };
 
 callMyName();
+
+
+
+add = function (number1 ,number2)
+{
+	return (parseInt(number1)+parseInt(number2));
+}
+
+console.log(add(1, 5));
+
